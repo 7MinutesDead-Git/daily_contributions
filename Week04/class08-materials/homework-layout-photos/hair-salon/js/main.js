5ccc61879f369eabc25dc549325fdd817df48f19
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-02-18 19:30:53 -0600

    Begin implementing background swapping functionality

--- Week04/class08-materials/homework-layout-photos/hair-salon/js/main.js
+++ Week04/class08-materials/homework-layout-photos/hair-salon/js/main.js
@@ -1,1 +1,11 @@
+caroselButtons = document.querySelectorAll('.btn-carosel')
+heroBackground = document.querySelector('.hero-bg-toggle')
 
+caroselButtons.forEach(btn => btn.addEventListener('click', e => {
+    if (heroBackground.style.opacity === '1'){
+        heroBackground.style.opacity = '0'
+        return
+    }
+    heroBackground.style.opacity = '1'
+    console.log('boop')
+}))
