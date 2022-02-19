3b2d3140b2c058d007e3a267846e80b879dbc259
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-02-19 10:08:39 -0600

    Refactor for readability and maintainability.

--- Week06/class12/background-picker/js/main.js
+++ Week06/class12/background-picker/js/main.js
@@ -11,6 +11,7 @@ function getRandomAngle() {
 
 // Update the body background and text colors.
 function updateColors(bgColor, textColor) {
+    gamerChair.style.filter = `hue-rotate(${getRandomAngle()}deg)`
     document.body.style.backgroundColor = bgColor
     // This is to avoid making the text color identical to the background color.
     document.body.style.color = textColor !== bgColor ? textColor : '#fff'
@@ -18,29 +19,44 @@ function updateColors(bgColor, textColor) {
     unorderedListItems.forEach(li => {
         li.style.color = bgColor
     })
-    gamerChair.style.filter = `hue-rotate(${getRandomAngle()}deg)`
 }
 
-// Add click event listeners to background change buttons.
-function makeItemsClickable(listItems){
-    listItems.forEach(li => li.addEventListener('click', e => {
+// Add event listener to update body background and text colors on click.
+function styleOnClick(btn, listItems) {
+    btn.addEventListener('click', e => {
         const bg_color = getComputedStyle(e.target).backgroundColor
-        const text_color = getComputedStyle(getRandomItem(unorderedListItems)).backgroundColor
+        const text_color = getComputedStyle(getRandomItem(listItems)).backgroundColor
         updateColors(bg_color, text_color)
-    }))
+    })
+}
+
+// Add event listener to buttons for fast gold border on hover.
+function styleOnMouseEnter(btn) {
+    btn.addEventListener('mouseenter', e => {
+        btn.style.color = 'gold'
+        btn.style.transitionDuration = '0.1s'
+    })
+}
+
+// Add event listener to buttons for slow border fade when mouse leaves.
+function styleOnMouseOut(btn) {
+    btn.addEventListener('mouseout', e => {
+        btn.style.transitionDuration = '0.7s'
+        btn.style.color = getComputedStyle(document.body).backgroundColor
+    })
+}
+
+// Add all event listeners for each background change button.
+function createEventListeners(listOfButtons) {
+    listOfButtons.forEach(btn => {
+        styleOnClick(btn, listOfButtons)
+        styleOnMouseEnter(btn, listOfButtons)
+        styleOnMouseOut(btn, listOfButtons)
+    })
 }
 
 
 // Start here. ------------------------------------------
 const unorderedListItems = document.querySelectorAll('li')
 const gamerChair = document.querySelector('img')
-makeItemsClickable(unorderedListItems)
-
-unorderedListItems.forEach(li => li.addEventListener('mouseenter', e => {
-    li.style.color = 'gold'
-    li.style.transitionDuration = '0.1s'
-}))
-unorderedListItems.forEach(li => li.addEventListener('mouseout', e => {
-    li.style.transitionDuration = '0.7s'
-    li.style.color = getComputedStyle(document.body).backgroundColor
-}))
+createEventListeners(unorderedListItems)
