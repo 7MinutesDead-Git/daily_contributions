6bd4f63b4373f952453454def254bfea773e9e98
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-02-19 20:47:04 -0600

    Add explanations.

--- Week06/class12/simple-calculator/js/main.js
+++ Week06/class12/simple-calculator/js/main.js
@@ -1,13 +1,6 @@
-// Variables -----------------------------------------------------------------
-const listItems = document.querySelectorAll('li')
-let runningTotal = 0
-// Get the initial styles of the list items so we can easily reset after modification.
-const baseStyle = getComputedStyle(listItems[0])
-
-
 // Functions -----------------------------------------------------------------
 
-// Apply flash-like animation to elements passed in when called.
+// Apply flash-like animation to array of elements passed in when called.
 // Reset styles to baseStyle after animation is complete.
 function flashOnPress(elementsToFlash) {
   for (const element of elementsToFlash){
@@ -41,8 +34,11 @@ function vibrate(element) {
 // Setup all event listeners for each button.
 function setupEventListeners(listItems) {
   for (const li of listItems) {
+    // This is so we can create "calculator" functionality based on
+    // the numbers in the buttons themselves. No need for button IDs.
     const value = parseFloat(li.innerText)
 
+    // --------------------------------
     li.addEventListener('click', e => {
       if (value === 0) {
         runningTotal = value
@@ -54,9 +50,10 @@ function setupEventListeners(listItems) {
       flashOnPress(elementsToFlash)
     })
 
+    // --------------------------------
     li.addEventListener('mouseenter', e => {
       const vibrationTimer = setInterval(vibrate.bind(null, li), 30)
-
+      // ------------------------------
       // Nesting this allows us to clear the interval when the mouse leaves the element.
       // Surely there's a better way to do this.
       li.addEventListener('mouseleave', e => {
@@ -70,4 +67,8 @@ function setupEventListeners(listItems) {
 
 
 // Start here. ---------------------------------------------------------------
+const listItems = document.querySelectorAll('li')
+let runningTotal = 0
+// Get the initial styles of the list items so we can easily reset after modification.
+const baseStyle = getComputedStyle(listItems[0])
 setupEventListeners(listItems)
