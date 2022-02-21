c8135329ed6e7c1dd06170dc3677ea9ec5c38b87
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-02-20 20:24:12 -0600

    Complete assignment.

--- Week06/class12/class-weekend-boring/js/main.js
+++ Week06/class12/class-weekend-boring/js/main.js
@@ -1,10 +1,22 @@
-document.querySelector('#check').addEventListener('click', check)
-
 function check() {
+  const day = document.querySelector('#day').value.toLowerCase()
+  const placeToSee = document.querySelector('#placeToSee')
 
-  const day = document.querySelector('#day').value
-
-  //Conditionals go here
-
-
+  if (classDays.includes(day)) {
+    placeToSee.innerText = 'class time!'
+  }
+  if (weekendDays.includes(day)) {
+    placeToSee.innerText = 'weekend time!'
+  }
+  if (boringDays.includes(day)) {
+    placeToSee.innerText = "Ehh.. it's a day!"
+  }
+  else {
+    placeToSee.innerText = 'Not a valid day!'
+  }
 }
+
+document.querySelector('#check').addEventListener('click', check)
+const classDays = ['tuesday', 'thursday']
+const weekendDays = ['friday', 'saturday', 'sunday']
+const boringDays = ['monday', 'wednesday']
