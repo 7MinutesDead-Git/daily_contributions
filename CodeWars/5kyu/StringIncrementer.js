ab1bd2e40ff07d9a603f4853ac7cf7b7bb53b2b9
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-03-17 04:26:42 -0500

    Add explanations.

--- CodeWars/5kyu/StringIncrementer.js
+++ CodeWars/5kyu/StringIncrementer.js
@@ -9,9 +9,9 @@ function incrementString (strng) {
     }
 
     const numLength = number.toString().length
+    // We need to explicitly declare base 10 or javascript defaults to base 8 for numbers with leading zeros.
     number = (parseInt(number, 10) + 1).toString()
-
-    // This is so we can maintain any leading zeros present.
+    // This is so we can maintain any leading zeros that should still be present after incrementing.
     while (number.length < numLength) {
         number = `0${number}`
     }
