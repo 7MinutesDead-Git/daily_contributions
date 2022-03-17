dbeeeb3060678290d773b35915cff773514214a1
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-03-17 03:49:28 -0500

    Initial Commit.

---
+++ CodeWars/5kyu/HumanReadableTime.js
@@ -0,0 +1,21 @@
+function humanReadable (seconds) {
+    let minutes = 0
+    let hours = 0
+
+    const minutesOverflow = seconds / 60
+    if (minutesOverflow >= 1) {
+        minutes += minutesOverflow
+        minutes = Math.floor(minutes)
+        seconds = seconds % 60
+    }
+    const hoursOverflow = minutes / 60
+    if (hoursOverflow >= 1) {
+        hours += hoursOverflow
+        hours = Math.floor(hours)
+        minutes = minutes % 60
+    }
+    hours = hours.toString().length === 1 ? `0${hours}` : hours
+    minutes = minutes.toString().length === 1 ? `0${minutes}` : minutes
+    seconds = seconds.toString().length === 1 ? `0${seconds}` : seconds
+    return `${hours}:${minutes}:${seconds}`
+}
