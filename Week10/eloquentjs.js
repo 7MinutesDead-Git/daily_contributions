8c518568a4242f4c56140f116b3b9373c4cbdb81
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-03-22 00:39:06 -0500

    Solve array and node problem.

--- Week10/eloquentjs.js
+++ Week10/eloquentjs.js
@@ -1,6 +1,9 @@
 // https://eloquentjavascript.net/04_data.html#i_8ZspxiCEC/
 
+
+// -------------------------------------------------------------
 // The sum of a range.
+// https://eloquentjavascript.net/04_data.html#i_8ZspxiCEC/
 function range(start, end, step = 1) {
     const result = []
     while (start <= end) {
@@ -18,4 +21,81 @@ const testArray = range(0, 100, 5)
 console.log(testArray)
 console.log(sum(testArray))
 
+
+// -------------------------------------------------------------
 // Reversing an array.
+// https://eloquentjavascript.net/04_data.html#i_6xTmjj4Rf5
+function reverseArray(array) {
+    const result = []
+    for (let i = array.length - 1; i >= 0; i--) {
+        result.push(array[i])
+    }
+    return result
+}
+console.log(reverseArray([1, 2, 3, 4, 5]))
+
+function reverseInPlace(array) {
+    const midpoint = array.length / 2
+    let start = 0
+    let end = array.length - 1
+
+    while (start <= midpoint) {
+        [array[start], array[end]] = [array[end], array[start]]
+        start++
+        end--
+    }
+}
+
+const reverseMe = [1, 2, 3, 4, 5, 6]
+reverseInPlace(reverseMe)
+console.log(reverseMe)
+
+
+// -------------------------------------------------------------
+// https://eloquentjavascript.net/04_data.html#i_nSTX34CM1M
+// A List.
+class ListNode {
+    constructor(value = 0, next = null) {
+        this.value = value
+        this.next = next
+    }
+}
+
+function arrayToList(array){
+    let node = new ListNode()
+    // Building it backwards is simpler than building it forwards.
+    // I think to build it forwards I would need a recursive approach.
+    for (let i = array.length - 1; i > 0; i--) {
+        node.value = array[i]
+        node = new ListNode(array[i-1], node)
+    }
+    return node
+}
+
+function listToArray(node) {
+    const result = []
+    while (node.next !== null) {
+        result.push(node.value)
+        node = node.next
+    }
+    result.push(node.value)
+    return result
+}
+
+function isArrayValid(arrayOne, arrayTwo) {
+    for (let i = 0; i < numArray.length; i++)
+        if (arrayOne[i] !== arrayTwo[i])
+            return false
+    return true
+}
+
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const linkedList = arrayToList(numArray)
+const backToArray = listToArray(linkedList)
+console.log(isArrayValid(numArray, backToArray))
+
+
+// -------------------------------------------------------------
+// https://eloquentjavascript.net/04_data.html#i_IJBU+aXOIC
+// Deep comparison.
+
