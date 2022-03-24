be012fd52ceb4fb9d749791a413ca2bf232be5a6
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-03-23 19:36:06 -0500

    Initial commit

---
+++ CodeWars/4kyu/Permutations.js
@@ -0,0 +1,42 @@
+function permuteMe(string) {
+    const array = Array.from(string)
+    const length = array.length
+    const seen = Array(length)
+    const variation = Array(length)
+
+    // Generator function for yielding each permutation as requested.
+    // I don't really think I need to use a generator but I wanted to practice anyway.
+    return function* backtracking(position) {
+        if (position === length)
+            yield variation.slice().join('')
+        else
+            for (let i = 0; i < length; ++i)
+                if (!seen[i]) {
+                    seen[i] = true
+                    variation[position] = array[i]
+                    yield* backtracking(position + 1)
+                    seen[i] = false
+                }
+    }(0);
+}
+
+function unpackPermutations(generator, result = []) {
+    let item = generator.next()
+
+    if (item['done'] === false) {
+        result.push(item['value'])
+        unpackPermutations(generator, result)
+    }
+    return result
+}
+
+function permutations(string) {
+    const generator = permuteMe(string)
+    // To be honest I'm not sure if the problem is calling for permutations or combinations,
+    // since some test cases are resolved by removing duplicates, but I can't say for sure if I'm
+    // accidentally generating excess duplicates because I'm really stumbling my way through this problem.
+    // Oh well, this works!
+    return [...new Set(unpackPermutations(generator))]
+}
+
+console.log(permutations('aabb'))
