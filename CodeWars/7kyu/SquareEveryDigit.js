7fbf577969b371e40e9c3412ab8bab639daa6235
7MinutesDead-Git <7minutesdead@gmail.com>
2022-04-03 10:05:31 -0500

    initial commit

---
+++ CodeWars/7kyu/SquareEveryDigit.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript
+function squareDigits(num){
+    let result = ''
+    for (const number of num.toString())
+        result += `${number*number}`
+
+    return Number(result)
+}
