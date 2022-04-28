9a9fb69823c5f755c6a11947674ac8507d324cec
7MinutesDead-Git <7minutesdead@gmail.com>
2022-04-28 00:00:14 -0500

    Initial commit

---
+++ CodeWars/5kyu/Rot13.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript
+// I'm kinda proud of this one!
+function rot13(message){
+    const alphabet = [...'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ']
+    const input = [...message]
+    const result = []
+
+    for (const char of input) {
+        let index = alphabet.indexOf(char)
+
+        if (index === -1)
+            result.push(char)
+        else {
+            const encodedIndex = (index + 26) % alphabet.length
+            result.push(alphabet[encodedIndex])
+        }
+    }
+    return result.join("")
+}
