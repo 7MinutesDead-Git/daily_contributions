2485b0c7ec7f0a56d827128aaf129d4242316886
7MinutesDead-Git <7minutesdead@gmail.com>
2022-04-29 14:15:30 -0500

    add todo list as pseudo-code.

--- CodeWars/3kyu/BattleshipFieldValidator.js
+++ CodeWars/3kyu/BattleshipFieldValidator.js
@@ -1,5 +1,31 @@
 // https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/javascript
 // TODO.
+// initialise the ship map with pairs of (size, amount of ships) values
+//
+// initialise your map[12][12]:
+//   for every place at row and column coordinate of 0 or 11 (the border)
+//     mark it as visited
+//   for every other place
+//      mark it as not visited
+//      fill it with either ship or ocean tile from your input
+//
+// for each row from 1 to 10
+//   for each column from 1 to 10
+//     if that place has not been visited yet
+//       mark that place as visited
+//       if that place is a ship tile
+//         check the places to the "right" (bigger column numbers)
+//           ... and bottom (bigger "row" numbers)
+//           until you hit a visited or ocean tile
+//         the amount of ship tiles checked (including the first) is current ship's length
+//         decrease the amount of ships of that length in the ship map by one
+//         mark all ship tiles of the current ship as visited
+//         mark all tiles surrounding those ship tiles as visited
+//
+// if the ship map includes any pairs with non-zero (including negative) amount of ships
+//   the map is invalid
+// else
+//   the map is valid
 // --------------------------------------------------
 function isValidPlacement(hitPosition) {
     let cornerCheck = false
