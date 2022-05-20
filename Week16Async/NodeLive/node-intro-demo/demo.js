9ceb5ac9c7761413d78122a33dff31d5986f1a3a
7MinutesDead-Git <7minutesdead@gmail.com>
2022-05-20 18:15:12 +0800

    Add notes to Node demo.

--- Week16Async/NodeLive/node-intro-demo/demo.js
+++ Week16Async/NodeLive/node-intro-demo/demo.js
@@ -1,9 +1,13 @@
 const http = require('http')
 const fs = require('fs')
+
+// Create server to listen on port 8000.
 http.createServer((req, res) => {
+  // Read the file and write it to the response object for the client to receive.
   fs.readFile('demofile.html', (err, data) => {
     res.writeHead(200, {'Content-Type': 'text/html'})
     res.write(data)
     res.end()
   })
+// localhost:8000
 }).listen(8000)
