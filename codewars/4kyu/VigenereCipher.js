7e969ef9bc4d0d9da0c709ae2a4c2ecde71f3091
7MinutesDead-Git <7minutesdead@gmail.com>
2022-06-08 13:27:45 +0800

    begin encode and decode implementation

--- codewars/4kyu/VigenereCipher.js
+++ codewars/4kyu/VigenereCipher.js
@@ -5,20 +5,41 @@ class VigenèreCipher {
         this.abc = [...abc]
         this.result = []
     }
-    // TODO
-    getCipherIndex() {
 
-    }
-    // TODO
     encode(str) {
         const input = [...str]
-        let encodeIndex = 0
         for (const [index, letter] of input.entries()) {
-            encodeIndex = this.getCipherIndex(index)
+            this.cipherize(index, letter, true)
+        }
+        return this.result.join('')
+    }
+    decode(str) {
+        const input = [...str]
+        for (const [index, letter] of input.entries()) {
+            this.cipherize(index, letter, false)
+        }
+        return this.result.join('')
+    }
+
+    cipherize(index, letter, encrypt) {
+        if (this.abc.includes(letter)) {
+            // Essentially, our key loops across the entire input.
+            const keyIndex = this.key.length % index
+            if (isNaN(keyIndex)) {
+                throw new Error(`Key index came out NaN for some reason.. Index ${index}, Letter ${letter}`)
+            }
+            const encodedIndex = this.abc.indexOf(this.key[keyIndex])
+            let encodedLetter
+            encrypt ?
+                encodedLetter = this.abc[encodedIndex + this.abc.indexOf(letter)] :
+                encodedLetter = this.abc[this.abc.indexOf(letter) - encodedIndex]
+            this.result.push(encodedLetter)
+        } else {
+            this.result.push(letter)
         }
     }
-    // TODO
-    decode(str) {
-
-    }
 }
+
+const test = new VigenèreCipher('password', 'abcdefghijklmnopqrstuvwxyz')
+console.log(test.encode('codewars'))
+console.log(test.decode('rovwsoiv'))
