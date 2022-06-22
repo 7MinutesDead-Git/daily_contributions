8dc06b938ade50748aab85c7e5e9217a8f4f1ae2
7MinutesDead-Git <7minutesdead@gmail.com>
2022-06-22 21:24:00 +0800

    Refactor server and mongoDB connection to class object

--- follow_along/week16async_node_express/express_mongo/server.js
+++ follow_along/week16async_node_express/express_mongo/server.js
@@ -5,107 +5,131 @@ const express = require('express')
 const fs = require('fs')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 
-// ------------------------------------------------------------
+// --------------------------------------------------------------------
 // Global Variables
 const PORT = process.env.PORT || 3000
 
-// ------------------------------------------------------------
+// --------------------------------------------------------------------
+// Static functions
 function delay(ms) {
   return new Promise((resolve, reject) => {
     setTimeout(resolve, ms)
   })
 }
 
-// ------------------------------------------------------------
-function startServer() {
-  const app = express()
-  connectToMongo(app)
-}
+// --------------------------------------------------------------------
+// A class to run and manage an Express server and its MongoDB connection.
+class ExpressServer {
+  // ------------------------------------------------------------
+  // Private properties
+  #credentialsURI
+  #mongoClientSetup
 
-function setupMiddleware(app) {
-  // We don't need to use body-parser anymore as of Express 4.16.0+
-  // Used to parse JSON bodies.
-  app.use(express.json())
-  // Used to parse URL-encoded bodies using qs (query string) library.
-  app.use(express.urlencoded({ extended: true }))
-  // Since we can't send multiple files with sendFile, and we want to serve
-  // things like index.html and its stylesheet, we can use static instead.
-  // This will serve everything placed in the "public" directory.
-  app.use(express.static('public'))
-}
-
-function setupGetRoutes(app) {
-  // // Keeping for future reference, but this is now handled by the
-  // // static middleware above.
-  // app.get('/', (req, res) => {
-  //   res.sendFile(__dirname + '/index.html')
-  // })
-}
-function setupPostRoutes(app) {
-  app.post('/add', (req, res) => {
-    const record = req.body
-    console.log(`Submitted new drink: ${record.name}: "${record.instructions}"`)
-    console.log(record)
-    res.send('Drink submitted! Please wait a bit for the submission to be reviewed.')
-  })
-}
-
-function setupBadRoute(app) {
-  app.all('*', (req, res) => {
-    console.log(`🐡 Bad request from ${req.ip} ==> ${req.url} 🐡`)
-    res.send('🐡 404 🐡')
-  })
-}
-
-// ------------------------------------------------------------
-async function setupMongoDBConnection() {
-  const credentialsURI = process.env.mongo_db_hw_uri
-  const mongoClientSetup = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1
+  // ------------------------------------------------------------
+  // Initial setup
+  constructor() {
+    this.app = express()
+    this.#credentialsURI = process.env.mongo_db_hw_uri
+    this.#mongoClientSetup = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1
+    }
+    this.db = null
+    this.collection = null
+    this.docCount = null
   }
 
-  return new Promise((resolve, reject) => {
-    let client
-    try {
-      client = new MongoClient(credentialsURI, mongoClientSetup)
-    }
-    catch (err) {
-      console.log(`🙈🔥 Problem creating mongoDB client. Check URL, or credentials: ${err}`)
-      reject(err)
-    }
-    resolve(client)
-  })
-}
+  // ------------------------------------------------------------
+  // Startup the Express server on the designated port.
+  startServer() {
+    this.#setupMiddleware()
+    this.#setupPostRoutes()
+    this.#setupGetRoutes()
+    this.#setupBadRoute()
 
-// ------------------------------------------------------------
-async function connectToMongo(app) {
-  const client = await setupMongoDBConnection()
-
-  try {
-    await client.connect()
-    console.log('🦆 Connected to MongoDB Cloud!')
-
-    const database = client.db("recipe-db")
-    const collection = database.collection("recipe-collection")
-    const docCount = await collection.countDocuments({})
-    console.log(`🦆 Document count: ${docCount}`)
-
-    setupMiddleware(app)
-    // -------------------------------
-    app.listen(PORT, () => {
+    this.app.listen(PORT, () => {
       console.log(`🐡 Node up on port ${PORT} 🐡`)
     })
-    setupGetRoutes(app)
-    setupPostRoutes(app)
-    setupBadRoute(app)
+  }
 
-  } finally {
-    console.log('🦍 Closing connection to MongoDB Cloud')
-    await client.close()
+  // ------------------------------------------------------------
+  // Setup helper methods
+  #setupMiddleware() {
+    // We don't need to use body-parser anymore as of Express 4.16.0+
+    // Used to parse JSON bodies.
+    this.app.use(express.json())
+    // Used to parse URL-encoded bodies using qs (query string) library.
+    this.app.use(express.urlencoded({ extended: true }))
+    // Since we can't send multiple files with sendFile, and we want to serve
+    // things like index.html and its stylesheet, we can use static instead.
+    // This will serve everything placed in the "public" directory.
+    this.app.use(express.static('public'))
+  }
+  #setupGetRoutes() {
+    // // Keeping for future reference, but this is now handled by the
+    // // static middleware above.
+    // app.get('/', (req, res) => {
+    //   res.sendFile(__dirname + '/index.html')
+    // })
+  }
+  #setupPostRoutes() {
+    this.app.post('/add', (req, res) => {
+      const record = req.body
+      console.log(`Submitted new drink: ${record.name}: "${record.instructions}"`)
+      console.log(record)
+      res.send('Drink submitted! Please wait a bit for the submission to be reviewed.')
+    })
+  }
+  #setupBadRoute() {
+    this.app.all('*', (req, res) => {
+      console.log(`🐡 Bad request from ${req.ip} ==> ${req.url} 🐡`)
+      res.send('🐡 404 🐡')
+    })
+  }
+
+  // ------------------------------------------------------------
+  // Returns a promise that resolves to a MongoDB client.
+  async #setupMongoDBConnection() {
+    return new Promise((resolve, reject) => {
+      let client
+      try {
+        client = new MongoClient(this.#credentialsURI, this.#mongoClientSetup)
+      }
+      catch (err) {
+        console.log(`🙈🔥 Problem creating mongoDB client. Check URL, or credentials: ${err}`)
+        reject(err)
+      }
+      resolve(client)
+    })
+  }
+  // ------------------------------------------------------------
+  // Establish connection to mongoDB and get our database, collections and
+  // document count.
+  async createMongoConnection() {
+    const client = await this.#setupMongoDBConnection()
+
+    try {
+      await client.connect()
+      console.log('🦆 Connected to MongoDB Cloud!')
+      this.db = client.db("recipe-db")
+      this.collection = this.db.collection("recipe-collection")
+      this.docCount = await this.collection.countDocuments({})
+      console.log(`🦆 Document count: ${this.docCount}`)
+    }
+    catch (err) {
+      console.log(`🙈🔥 Problem connecting to mongoDB: ${err}`)
+    }
+    finally {
+      // console.log('🦍 Closing connection to MongoDB Cloud')
+      // client.close()
+    }
   }
 }
 
-// ------------------------------------------------------------
-startServer()
+
+// --------------------------------------------------------------------
+// Start the server.
+const server = new ExpressServer()
+server.startServer()
+server.createMongoConnection()
