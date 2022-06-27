c61795df30a4e444e6ab248c3378d9f4a8685574
7MinutesDead-Git <7minutesdead@gmail.com>
2022-06-27 18:26:01 +0800

    Add put and delete fetch functionality to buttons

--- follow_along/week16async_node_express/express_mongo/public/js/main.js
+++ follow_along/week16async_node_express/express_mongo/public/js/main.js
@@ -1,17 +1,40 @@
-const updateButton = document.querySelector('.btn.edit')
-const deleteButton = document.querySelector('.btn.delete')
+const recipes = document.querySelector('.recipe-list')
 
-updateButton.addEventListener('click', async (e) => {
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+recipes.addEventListener('click', async (e) => {
+    // Select the child h3 element of the clicked element. This contains the recipe name.
+    const recipeName = e.target.parentElement.querySelector('h3').innerText
+    let requestMethod = 'GET'
+
+    if (e.target.classList.contains('edit')) {
+        requestMethod = 'PUT'
+    }
+    if (e.target.classList.contains('delete')) {
+        requestMethod = 'DELETE'
+    }
+
     // Send PUT request to update the post with new data
     const response = await fetch('/recipes', {
-        method: 'PUT',
+        method: requestMethod,
         headers: {
             'Content-Type': 'application/json'
         },
         body:JSON.stringify({
-            name: 'Updated Recipe',
-            ingredients: 'Updated Ingredients',
-            instructions: 'Updated Instructions'
+            name: recipeName,
+            ingredients: 'successfully edited ingredients',
+            instructions: 'successfully edited ingredients'
         })
     })
+
+    if (response.ok) {
+        const updatedRecipe = await response.json()
+        console.log(`Updated recipe with ${requestMethod} request!`)
+        console.log(updatedRecipe)
+        wait(1000).then(() => window.location.reload())
+    }
 })
