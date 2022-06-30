ceb96fc0595aa3eba4699fcc5aeb97772d02e8f7
7MinutesDead-Git <7minutesdead@gmail.com>
2022-07-01 02:50:49 +0800

    Fix incrementer location causing off-by-one error on half of tests

--- codewars/6kyu/EqualSidesOfArray.js
+++ codewars/6kyu/EqualSidesOfArray.js
@@ -1,13 +1,16 @@
-const testArray = [20,10,-80,10,10,15,35]
+const array1 = [20,10,-80,10,10,15,35]
+const array2 = [1,2,3,4,3,2,1]
+const arrays = [array1, array2]
 
 // https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/javascript
 // TODO: Fix.
 function findEvenIndex(arr) {
     let leftTotal = 0
     let rightTotal = 1
-    let mid = 0
+    let mid = -1
 
     while (leftTotal !== rightTotal) {
+        mid++
         // Remember that slice end index ends before the given index.
         // Also that leaving out the end index, means the end index will be the very last index.
         leftTotal = arr.slice(0, mid).reduce((a, b) => a + b, 0)
@@ -17,9 +20,10 @@ function findEvenIndex(arr) {
             mid = -1
             break
         }
-        mid++
     }
     return mid
 }
 
-console.log(findEvenIndex(testArray))
+for (const array of arrays) {
+    console.log(findEvenIndex(array))
+}
