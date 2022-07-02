3762a654b4e7d2e73395473e004efe9655155bce
7MinutesDead-Git <7minutesdead@gmail.com>
2022-07-03 03:11:34 +0800

    Detect Panagram coding challenge: initial commit

---
+++ codewars/6kyu/DetectPangram.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
+
+function createLetterCounterMap() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    const counts = new Map()
+    for (const letter of alphabet) {
+        counts.set(letter, 0)
+    }
+    return counts
+}
+
+function isPangram(string) {
+    const counts = createLetterCounterMap()
+
+    for (let char of string) {
+        char = char.toLowerCase()
+        counts.set(char, counts.get(char) + 1)
+    }
+
+    for (const [letter, count] of counts) {
+        if (count < 1)
+            return false
+    }
+    return true
+}
