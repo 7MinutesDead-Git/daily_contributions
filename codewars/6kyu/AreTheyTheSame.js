4b31cc8ce4178522180f73b6cd43311e86cca4d6
7MinutesDead-Git <7minutesdead@gmail.com>
2022-07-04 03:20:04 +0800

    Swap let for const

--- codewars/6kyu/AreTheyTheSame.js
+++ codewars/6kyu/AreTheyTheSame.js
@@ -3,7 +3,7 @@ function comp(array1, array2) {
     if (!array1 || !array2)
         return false
 
-    for (let num of array1) {
+    for (const num of array1) {
         const i = array2.indexOf(num ** 2)
         if (i !== -1)
             array2.splice(i, 1)
