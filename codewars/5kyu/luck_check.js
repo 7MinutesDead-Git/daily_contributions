6f53e739a9231cf59daca1b2377dd87803e22147
7MinutesDead-Git <7minutesdead@gmail.com>
2022-07-21 20:49:54 +0800

    Add helpful comments. Fix typo in error message.

--- codewars/5kyu/luck_check.js
+++ codewars/5kyu/luck_check.js
@@ -1,5 +1,9 @@
 // https://www.codewars.com/kata/5314b3c6bb244a48ab00076c/train/javascript
 function getSums(array) {
+    // Since the tests expect an error to be thrown when a string doesn't represent
+    // a number, we can just throw the error from inside our reduce function here.
+    // This ensures we only need to loop over our entire string/array once,
+    // so we stick to O(n).
     return array.reduce((sum, num) => {
         if (isNaN(num) || num === " ")
             throw new Error(`${num} in ${array} is not a number.`)
@@ -9,15 +13,18 @@ function getSums(array) {
 
 function luckCheck(ticket) {
     ticket = ticket.split("")
-    let mid = ticket.length / 2
+    const mid = ticket.length / 2
     const lengthIsEven = ticket.length % 2 === 0
 
     let left = ticket.slice(0, mid)
+    // Null here is redundant, but if any bugs started from here,
+    // we would know null was intentional.
+    // Otherwise it would return undefined if we simply declare it as "let right"
     let right = null
 
     lengthIsEven ?
-        right = ticket.slice(mid)
-        : right = ticket.slice(mid+1)
+        right = ticket.slice(mid) :
+        right = ticket.slice(mid+1)
 
     left = getSums(left)
     right = getSums(right)
