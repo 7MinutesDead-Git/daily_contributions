b655eecff1ae294c0f803bfd2c89545cfff506ea
7MinutesDead-Git <7minutesdead@gmail.com>
2022-08-07 22:18:11 +0800

    AE: Validate Subsequence challenge: Initial commit

---
+++ codewars/AE/arrays/validate_subsequence.js
@@ -0,0 +1,20 @@
+// https://www.algoexpert.io/questions/validate-subsequence
+function isValidSubsequence(array, sequence) {
+    // The amount of elements from sequence we've found in array.
+    // We'll say that if we've found them all (sequence.length), then return true.
+    let found = 0
+    // The element from sequence we're currently searching for.
+    // We'll start with the first.
+    let target = sequence[0]
+
+    // We can do this in O(n), one pass through array.
+    for (const num of array) {
+        if (num === target) {
+            found++
+            // Found counter also happens to line up with the index
+            // we want to be tracking, so we'll use it as our moving index too.
+            target = sequence[found]
+        }
+    }
+    return found === sequence.length
+}
