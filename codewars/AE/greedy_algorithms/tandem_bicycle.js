961cd20c15acd624cd45bde010132c9efff7df12
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-14 14:11:26 -0500

    Refactor to sort reversed in one step, rather than sorting then reversing.

--- codewars/AE/greedy_algorithms/tandem_bicycle.js
+++ codewars/AE/greedy_algorithms/tandem_bicycle.js
@@ -3,19 +3,15 @@ function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
     // Initially forgot to use a/b for sorting, so nums sorted as strings
     // and messed up some test cases.
     redShirtSpeeds = redShirtSpeeds.sort((a, b) => a - b)
-    blueShirtSpeeds = blueShirtSpeeds.sort((a, b) => a - b)
 
-    if (fastest)
-        blueShirtSpeeds = blueShirtSpeeds.reverse()
+    // Reverse our sort if we're looking for the fastest, so we always
+    // start by pairing our largest with our smallest.
+    blueShirtSpeeds = fastest ?
+        blueShirtSpeeds.sort((a, b) => b - a) :
+        blueShirtSpeeds.sort((a, b) => a - b)
 
-    const result = redShirtSpeeds.reduce((total, redSpeed, i) => {
-        // For each speed pair, we must always choose the fastest.
-        total += Math.max(redSpeed, blueShirtSpeeds[i])
+    return redShirtSpeeds.reduce((total, speed, i) => {
+        total += Math.max(speed, blueShirtSpeeds[i])
         return total
     }, 0);
-
-    return result
 }
-
-// Do not edit the line below.
-exports.tandemBicycle = tandemBicycle;
