72e636c188463f5ccbcd3e2e622379dd80df3967
7MinutesDead-Git <7minutesdead@gmail.com>
2022-11-01 15:38:18 -0500

    Add example react hook to use state with local storage

---
+++ templates/react-hooks/local-storage-state.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react"
+
+export function useLocalStorageState(key: string, defaultValue: any) {
+    const [state, setState] = useState(() => {
+        const valueInLocalStorage = localStorage.getItem(key)
+        if (valueInLocalStorage) {
+            return JSON.parse(valueInLocalStorage)
+        }
+        return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state))
+    }, [key, state])
+
+    return [state, setState]
+}
