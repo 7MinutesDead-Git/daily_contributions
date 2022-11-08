5c0ab2a64e1e648c45ec29405422d2a2dad017cd
7MinutesDead-Git <7minutesdead@gmail.com>
2022-11-08 14:33:45 -0600

    HT: Find deleted num: Add sum trick & ts signatures

--- follow_along/week99/huntober_09.ts
+++ follow_along/week99/huntober_09.ts
@@ -2,16 +2,28 @@
 // One number might have deleted from it, then the remaining numbers were mixed.
 // Find the number that was deleted.
 
-function findDeletedNumber(arr, mixArr) {
-    if (arr.length === mixArr.length) {
+// This would be necessary of input arrays could be of any type.
+
+function findDeletedNumber(numArray: number[], mixedArray: number[]): number {
+    if (numArray.length === mixedArray.length) {
         return 0
     }
-
-    mixArr.sort((a, b) => a - b)
-    for (const [i, num] of arr.entries()) {
-        if (mixArr[i] !== num) {
+    mixedArray.sort((a, b) => a - b)
+    for (const [i, num] of numArray.entries()) {
+        if (mixedArray[i] !== num) {
             return num
         }
     }
     return 0
+}
+
+// Since the array is only ever made of numbers, and we only need to find one missing number,
+// really that's just the difference in sums between the two arrays. No sorting needed.
+function findDeletedNumberSumTrick(numArray: number[], mixedArray: number[]): number {
+    if (numArray.length === mixedArray.length) {
+        return 0
+    }
+    const numArraySum = numArray.reduce((a, b) => a + b, 0)
+    const mixedArraySum = mixedArray.reduce((a, b) => a + b, 0)
+    return numArraySum - mixedArraySum
 }
