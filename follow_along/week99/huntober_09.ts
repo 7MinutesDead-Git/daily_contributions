65e026750d252cc94a0db8714adf4cfdfc04568c
7MinutesDead-Git <7minutesdead@gmail.com>
2022-11-08 22:03:57 -0600

    HT: Find deleted num: Add hash map alternative & time complexity breakdowns

--- follow_along/week99/huntober_09.ts
+++ follow_along/week99/huntober_09.ts
@@ -4,10 +4,14 @@
 
 // O(n log n) from sorting.
 function findDeletedNumber(numArray: number[], mixedArray: number[]): number {
+    // O(1) guard
     if (numArray.length === mixedArray.length) {
         return 0
     }
+    // O(n log n) sort
     mixedArray.sort((a, b) => a - b)
+
+    // O(n) concurrent loop
     for (const [i, num] of numArray.entries()) {
         if (mixedArray[i] !== num) {
             return num
@@ -20,10 +24,34 @@ function findDeletedNumber(numArray: number[], mixedArray: number[]): number {
 // really that's just the difference in sums between the two arrays. No sorting needed.
 // O(n)
 function findDeletedNumberSumTrick(numArray: number[], mixedArray: number[]): number {
+    // O(1) guard
     if (numArray.length === mixedArray.length) {
         return 0
     }
+    // O(n) reduces
     const numArraySum = numArray.reduce((a, b) => a + b, 0)
     const mixedArraySum = mixedArray.reduce((a, b) => a + b, 0)
+    // O(1) diff
     return numArraySum - mixedArraySum
+}
+
+// Still O(n) since we'll need to loop through both arrays once.
+function findDeletedNumberHashMap(numArray: number[], mixedArray: number[]): number {
+    // O(1) guard
+    if (numArray.length === mixedArray.length) {
+        return 0
+    }
+    // O(n) building map
+    const seen = mixedArray.reduce((found, num) => {
+        found[num] = true
+        return found
+    }, {})
+
+    // O(n) checking original array
+    for (const num of numArray) {
+        // O(1) lookup
+        if (!seen[num])
+            return num
+    }
+    return 0
 }
