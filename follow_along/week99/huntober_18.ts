5f5345715cacfc4e9c835a2743108b3e7c4318ae
7MinutesDead-Git <7minutesdead@gmail.com>
2022-12-05 16:55:29 -0600

    HT: Count with map/object: Initial commit

---
+++ follow_along/week99/huntober_18.ts
@@ -0,0 +1,22 @@
+// Count all the occurring characters in a string.
+// If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+//
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+type Counts = {
+    [key: string]: number
+}
+function count(input: string): Counts {
+    const counts: Counts = {}
+
+    input.split("").reduce((count, char) => {
+        count[char] ? count[char]++ : count[char] = 1
+        return count
+    }, counts)
+
+    return counts
+}
+
+console.log(count("aba"))
+console.log(count("112233"))
+console.log(count(""))
