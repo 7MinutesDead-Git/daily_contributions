6fc1010d82f3a374bc67c0184890132e53d3262e
7MinutesDead-Git <7minutesdead@gmail.com>
2022-12-06 16:02:25 -0600

    HT: Number of jewels in stones: Initial commit

--- follow_along/week99/huntober_18.ts
+++ follow_along/week99/huntober_18.ts
@@ -1,6 +1,5 @@
 // Count all the occurring characters in a string.
 // If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
-//
 // What if the string is empty? Then the result should be empty object literal, {}.
 
 type Counts = {
---
+++ follow_along/week99/huntober_19.ts
@@ -0,0 +1,39 @@
+// You're given strings jewels representing the types of stones that are jewels,
+// and stones representing the stones you have. Each character in stones is a
+// type of stone you have. You want to know how many of the stones you have are also jewels.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+//
+// Example 1:
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+
+// Example 2:
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+
+// Constraints:
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+function numJewelsInStones(jewels: string, stones: string): number {
+    const stoneCounts = stones.split("").reduce((count, stone) => {
+        count[stone] ? count[stone]++ : count[stone] = 1
+        return count
+    }, {})
+
+    let result = 0
+
+    for (const jewel of jewels) {
+        if (stoneCounts[jewel]) {
+            result += stoneCounts[jewel]
+        }
+    }
+
+    return result
+}
+
+console.log(numJewelsInStones("aA", "aAAbbbb"))
+console.log(numJewelsInStones("aA", "aaaaAaAa"))
+console.log(numJewelsInStones("z", "ZZ"))
+console.log(numJewelsInStones("", ""))
