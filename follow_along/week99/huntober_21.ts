459bc364dfc17d0ca9a0d721cece3745389e24c7
7MinutesDead-Git <7minutesdead@gmail.com>
2022-12-14 16:54:30 -0600

    HT: ransom letter by words: Initial commit

---
+++ follow_along/week99/huntober_21.ts
@@ -0,0 +1,61 @@
+// Given a magazine of words and a ransom note, determine if it’s possible to “cut out”
+// and create the ransom note from the magazine words.
+//
+const magazine =
+ `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+ incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`;
+
+function lettersOnly(word: string) {
+    return word.replace(/^a-zA-Z]/g, '').trim().toLowerCase()
+}
+
+function ransomableByWords(magazine: string, note: string): boolean {
+    if (note.length === 0) {
+        return true
+    }
+    const ransomWords = note.split(" ").map(word => lettersOnly(word))
+    const availableWords = magazine.split(" ").map(word => lettersOnly(word))
+        .reduce((count, word) => {
+            count[word] ? count[word]++ : count[word] = 1
+            return count
+        }, {})
+
+    for (const word of ransomWords) {
+        if (availableWords[word]--) {
+            continue
+        }
+        return false
+    }
+
+    return true
+}
+
+console.log(ransomableByWords(magazine, "sit ad est sint")) // true
+console.log(ransomableByWords(magazine, "sit ad est love")) // false
+console.log(ransomableByWords(magazine, "sit ad est sint in in in")) // true
+console.log(ransomableByWords(magazine, "sit ad est sint in in in in")) // false
+console.log(ransomableByWords(magazine, "reprehenderit")) // true
+console.log(ransomableByWords(magazine, "")) // true
+console.log(ransomableByWords(magazine, "aliquip ex ea commodo")) // true
+
+
+// describe("Ransom Note", () => {
+//     it("Should return true", () => {
+//         assert.equal(ransomableByWords("sit ad est sint", magazine), true)
+//     })
+//
+//     it("Should return false", () => {
+//         assert.equal(ransomableByWords("sit ad est love", magazine), false);
+//     });
+//
+//     it("Should return true", () => {
+//         assert.equal(ransomableByWords("sit ad est sint in in", magazine), true);
+//     });
+//
+//     it("Should return false", () => {
+//         assert.equal(ransomableByWords("sit ad est sint in in in in", magazine), false);
+//     });
+// });
